ce105c29caabb5b03760581061518d4af8909b4e
Marco Caimi <mcaimi@redhat.com>
2023-11-23 12:05:08 +0100

    Update supported extensions list

--- pkg/cameraimport/variables.go
+++ pkg/cameraimport/variables.go
@@ -1,8 +1,8 @@
 package cameraimport
 
 var (
-  rawFormats [6]string = [6]string{"ARW", "RAF", "CR2", "CR3", "NEF", "ORF"};
+  rawFormats [7]string = [7]string{"ARW", "DNG", "RAF", "CR2", "CR3", "NEF", "ORF"};
   rasterFormats [5]string = [5]string{"JPG", "JPEG", "TIF", "TIFF", "PNG"};
-  videoFormats [4]string = [4]string{"MPG", "MP4", "AVI", "MKV"};
+  videoFormats [5]string = [5]string{"MOV", "MPG", "MP4", "AVI", "MKV"};
 )
 
